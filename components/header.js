82ee6a6381712dad004373b52e6f485e212873dc
Muhammad Ibrahim <rmibrahim00@gmail.com>
2025-02-28 22:30:27 +0100

    update: few style changes in header

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="py-4 px-28 bg-white shadow-lg max-md:px-10">
-      <div className="container mx-auto">
+      <div className="w-fit">
         <Link href="/">
           <h1 className="text-2xl font-bold text-primary">FAIR.FY</h1>
         </Link>
